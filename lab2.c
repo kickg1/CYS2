aa944ac26ebec6d27e5bb1c352c8d20688956514
Cys <kickg75@gmail.com>
2025-03-05 17:21:55 +0200

    Salut!

--- lab2.c
+++ lab2.c
@@ -1,20 +1,19 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+int function(int x)
+{
+	return x-2;
 }
 
-int main(){
-	int a=30, i=0;
-	
-	while (a>0){
+int main()
+{
+	int a=30,i=0;
+	while (a>0)
+	{
 		a=function(a);
 		i++;
 	}
 	printf("%d", i);
-	return 0;	
+	return 0;
 }
-
